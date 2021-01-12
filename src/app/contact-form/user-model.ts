5345185c9a4bc36c6d3dd5e99e0be498dd40ef06
Tyrone <tyronetang.xi@yahoo.com>
2021-01-12 19:31:33 +0800

    Auto-generated commit

--- src/app/contact-form/user-model.ts
+++
@@ -1,9 +0,0 @@
-export class User {
-    constructor(
-        public firstName: string,
-        public lastName: string,
-        public email: string,
-        public phone: number,
-        public category: string,
-    ){}
-}
