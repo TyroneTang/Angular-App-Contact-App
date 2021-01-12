af6ff1a89c47f82374f07bbdbde0f50ad2e56758
Tyrone <tyronetang.xi@yahoo.com>
2021-01-12 19:09:58 +0800

    first angular app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+
+import { ContactFormComponent } from './contact-form/contact-form.component';
+import { ContactListComponent } from './contact-list/contact-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { TreeComponent } from './tree/tree.component';
+
+const routes: Routes = [
+  { path: 'contact-form', component: ContactFormComponent },
+  { path: 'contact-list', component: ContactListComponent },
+  { path: 'tree', component: TreeComponent },
+  { path: '',   redirectTo: '/contact-list', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
